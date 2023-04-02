d0082dce62d56d482dc47351fd1ab9bf48ddaa16
coralavital <coralavital24@gmail.com>
2023-04-03 02:09:11 +0300

    application icon changed

--- lib/ui/home/account.dart
+++ lib/ui/home/account.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/dialog.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -13,6 +16,8 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePage extends State<ProfilePage> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,102 +26,88 @@ class _ProfilePage extends State<ProfilePage> {
         scrolledUnderElevation: 0,
         toolbarHeight: 15,
       ),
-      body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Account',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
+      body: StreamBuilder(
+        stream: _firestore
+            .collection('users')
+            .doc(_auth.currentUser!.uid)
+            .snapshots(),
+        builder: ((context, snapshot) {
+          if (!snapshot.hasData) {
+            return Container();
+          } else {
+            return Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: ListView(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Account',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-
-              // User card
-              // BigUserCard(
-              //   userName: "Babacar Ndong",
-              //   userProfilePic: AssetImage("assets/logo.png"),
-              //   cardActionWidget: SettingsItem(
-              //     icons: Icons.edit,
-              //     iconStyle: IconStyle(
-              //       withBackground: true,
-              //       borderRadius: 50,
-              //       backgroundColor: Colors.yellow[600],
-              //     ),
-              //     title: "Modify",
-              //     subtitle: "Tap to change your data",
-              //     onTap: () {
-              //       print("OK");
-              //     },
-              //   ),
-              // ),
-              // SettingsGroup(
-              //   items: [
-              //     SettingsItem(
-              //       onTap: () {},
-              //       icons: Icons.dark_mode_rounded,
-              //       iconStyle: IconStyle(
-              //         iconsColor: Colors.white,
-              //         withBackground: true,
-              //         backgroundColor: Colors.red,
-              //       ),
-              //       title: 'Dark mode',
-              //       subtitle: "Automatic",
-              //       trailing: Switch.adaptive(
-              //         value: false,
-              //         onChanged: (value) {},
-              //       ),
-              //     ),
-              //   ],
-              // ),
-              SettingsGroup(
-                items: [
-                  SettingsItem(
-                    onTap: () {},
-                    icons: Icons.info_rounded,
-                    iconStyle: IconStyle(
-                      backgroundColor: Colors.purple,
+                    SizedBox(
+                      height: 10,
                     ),
-                    title: 'Baby Information',
-                    // subtitle: "Learn more about Ziar'App",
-                  ),
-                ],
-              ),
-              // You can add a settings title
-              SettingsGroup(
-                items: [
-                  SettingsItem(
-                    onTap: () {
-                      FirebaseAuth.instance.signOut();
-                    },
-                    icons: Icons.exit_to_app_rounded,
-                    title: "Sign Out",
-                  ),
-                  SettingsItem(
-                    onTap: () {
-                      User user = FirebaseAuth.instance.currentUser!;
-                      user.delete();
-                    },
-                    icons: CupertinoIcons.delete_solid,
-                    title: "Delete account",
-                    titleStyle: TextStyle(
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
+                    SettingsGroup(
+                      items: [
+                        SettingsItem(
+                          onTap: () {
+                            // BabyDialog();
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return BabyDialog(
+                                    title: '${snapshot.data!['baby_details']['name']}',
+                                    message: [snapshot.data!['baby_details']['age'].toString(),snapshot.data!['baby_details']['height'].toString(), snapshot.data!['baby_details']['weight'].toString()],
+                                    detailType: ['Age', 'Height', 'Weight'],
+                                    iconPath: snapshot.data!['baby_details']['baby_icon'],
+                                  );
+                                });
+                          },
+                          icons: Icons.info_rounded,
+                          iconStyle: IconStyle(
+                            backgroundColor: Colors.purple,
+                          ),
+                          title: 'Baby Information',
+                          // subtitle: "Learn more about Ziar'App",
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            ],
-          )),
+                    // You can add a settings title
+                    SettingsGroup(
+                      items: [
+                        SettingsItem(
+                          onTap: () {
+                            FirebaseAuth.instance.signOut();
+                          },
+                          icons: Icons.exit_to_app_rounded,
+                          title: "Sign Out",
+                        ),
+                        SettingsItem(
+                          onTap: () {
+                            User user = FirebaseAuth.instance.currentUser!;
+                            user.delete();
+                          },
+                          icons: CupertinoIcons.delete_solid,
+                          title: "Delete account",
+                          titleStyle: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ));
+          }
+        }),
+      ),
     );
   }
 }
---
+++ lib/widgets/dialog.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'custom_field.dart';
+
+class BabyDialog extends StatelessWidget {
+  String title;
+  List<String> message;
+  List<String>? detailType;
+  String? iconPath;
+  BabyDialog(
+      {super.key,
+      required this.title,
+      required this.message,
+      this.detailType,
+      this.iconPath,});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Baby $title'),
+      icon: Image.network(iconPath!, height: 120, width: 120,),
+      content: ListView.builder(
+        itemCount: message.length,
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: ((context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: MyTextField(
+              fieldName: message[index],
+              fieldTitle: detailType![index],
+            ),
+          );
+        }),
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.pop(context, 'Cancel'),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () => {},
+          child: const Text('Edit'),
+        ),
+      ],
+    );
+  }
+}
